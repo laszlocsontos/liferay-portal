4de2342a1a9422c58993b01e07cb738fbfa5a5a8
Vilmos Papp <vilmos.papp@liferay.com>
2015-06-30 10:37:59 +0200

    LPS-49545 SF

--- modules/portal/portal-ldap/test/unit/com/liferay/portal/ldap/validator/LDAPUtilTest.java
+++ modules/portal/portal-ldap/test/unit/com/liferay/portal/ldap/validator/LDAPUtilTest.java
@@ -14,7 +14,6 @@
 
 package com.liferay.portal.kernel.ldap;
 
-import com.liferay.portal.ldap.validator.LDAPFilterValidatorImpl;
 import com.liferay.portal.security.ldap.LDAPFilterValidator;
 
 import org.junit.Assert;
